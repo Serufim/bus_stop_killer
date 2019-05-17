99f2f6f44eaed3964ff609e7d9730eed09af8dc4
serufim <serufim97@yandex.ru>
2019-05-18 02:58:46 +0800

    add logo and main menu

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -8,10 +8,11 @@ export  default class MainMenu extends Phaser.Scene {
         this.noPlayText = null
     }
     preload(){
-        this.load.image("")
+        this.load.image("title_bg",'assets/MenuBG.jpg')
     }
     create(){
         //  The score
+        this.add.image(0,0,'title_bg').setOrigin(0,0)
         this.scoreText = this.add.text(300, 16, 'Главное меню', { fontSize: '32px', fill: '#fff' })
     }
 }
